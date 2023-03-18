31cbfdd0fe50901e41b2df0d3398cfb3aa8e2ba5
Callum Oakley <hello@callumoakley.net>
2023-03-18 10:19:51 +0000

    Challenge #2: Unique ID Generation

--- src/gossip_glomers/uuid.clj
+++ src/gossip_glomers/uuid.clj
@@ -1,9 +1,9 @@
-(ns gossip-glomers.echo
+(ns gossip-glomers.uuid
   (:require [gossip-glomers.node :as node]))
 
 (defn handle [req]
   (case (:type req)
-    "echo" {:type "echo_ok" :echo (:echo req)}))
+    "generate" {:type "generate_ok" :id (java.util.UUID/randomUUID)}))
 
 (defn -main []
   (node/run (node/init) handle))
